1f7800d729ffb9ecb9e5b1d72392cc1e2b50b54d
nolan <nolanm1995@live.com>
2023-07-06 13:46:47 -0500

    Add error handler

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request
 
 from app import app
+from exceptions import APIError
 from pdf.controller import PDFController
 
 
@@ -31,6 +32,16 @@ def parse_pdf():
         },
     }
     return jsonify(response)
+
+@app.errorhandler(APIError)
+def handle_exception(err):
+    """Return custom JSON when APIError or its children are raised"""
+    response = {"error": err.description, "message": ""}
+    if len(err.args) > 0:
+        response["message"] = err.args[0]
+    # Add some logging so that we can monitor different types of errors
+    app.logger.error("{}: {}".format(err.description, response["message"]))
+    return jsonify(response), err.code
 
 
 if __name__ == "__main__":
