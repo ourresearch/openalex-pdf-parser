9b1038745d26088b1665ebea37fb883c388548f8
nolan <nolanm1995@live.com>
2023-07-13 09:48:35 -0500

    Add API key auth

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -5,3 +7,5 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["JSON_SORT_KEYS"] = False
+
+PDF_PARSER_API_KEY = os.getenv("PDF_PARSER_API_KEY")
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
+from functools import wraps
 from urllib.parse import quote
 
 from flask import jsonify, request, make_response
 
-from app import app
+from app import app, PDF_PARSER_API_KEY
 from exceptions import APIError
 from pdf.controller import PDFController
 
@@ -17,8 +18,17 @@ def home():
         }
     )
 
+def require_api_key(view_function):
+    @wraps(view_function)
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get('x-api-key') or request.args.get('api_key')
+        if not api_key or api_key != PDF_PARSER_API_KEY:
+            return jsonify({"message": "Invalid or missing API Key"}), 403
+        return view_function(*args, **kwargs)
+    return decorated_function
 
 @app.route('/parse')
+@require_api_key
 def parse_pdf():
     doi = request.args.get("doi")
     pdf_c = PDFController(doi)
@@ -36,6 +46,7 @@ def parse_pdf():
     return jsonify(response)
 
 @app.route('/view')
+@require_api_key
 def view_pdf():
     doi = request.args.get("doi")
     pdf_c = PDFController(doi)
